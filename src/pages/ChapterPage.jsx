9bb04d784fcba0b629de8c10b600ba05eab14f61
Saira Nasir <sairanasir853@gmail.com>
2025-12-10 22:07:46 +0500

    Fix ChapterPage export and update content

--- src/pages/ChapterPage.jsx
+++ src/pages/ChapterPage.jsx
@@ -3,11 +3,12 @@ import ChatWidget from "../components/ChatWidget";
 function ChapterPage() {
   return (
     <div>
-      <h1>Chapter Title saiera</h1>
-      <p>Chapter content...jjjjjjjjjjjjjj</p>
+      <h1>Chapter Title</h1>
+      <p>Chapter content...</p>
 
       {/* Chatbot Widget */}
       <ChatWidget />
     </div>
   );
 }
+export default ChapterPage;
